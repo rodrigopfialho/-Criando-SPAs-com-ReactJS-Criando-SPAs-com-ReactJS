797a7c6369871b78f5754eceffec8c787daa2600
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2022-08-21 17:16:59 -0300

    finalizado

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,101 +1,119 @@
-import { createContext, ReactNode, useState } from "react";
+import { differenceInSeconds } from 'date-fns'
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
+import {
+  ActionTypes,
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 
 interface CreateCycleData {
-    task: string;
-    minutesAmount: number;
+  task: string
+  minutesAmount: number
 }
-
-interface Cycle {
-    id: string;
-    task: string;
-    minutesAmount: number;
-    startDate: Date;
-    interruptedDate?: Date;
-    finishedDate?: Date;
-}
-
 
 interface CyclesContextType {
-    cycles: Cycle[]
-    activeCycle: Cycle | undefined;
-    activeCycleId: string | null;
-    amountSecondsPassed: number;
-    markCurrentCycleAsFinished: () => void;
-    setSecondsPassed: (seconds: number) => void;
-    createNewCycle: (data: CreateCycleData) => void;
-    interruptCurrentCycle: () => void;
+  cycles: Cycle[]
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  markCurrentCycleAsFinished: () => void
+  setSecondsPassed: (seconds: number) => void
+  createNewCycle: (data: CreateCycleData) => void
+  interruptCurrentCycle: () => void
 }
 
 export const CyclesContext = createContext({} as CyclesContextType)
 
 interface CyclesContextProviderProps {
-    children: ReactNode;
+  children: ReactNode
 }
 
-export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
-    const [cycles, setCycles] = useState<Cycle[]>([])
-    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0',
+      )
 
-    const activeCycle = cycles.find((cycles) => cycles.id === activeCycleId)
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+    },
+  )
 
-    function setSecondsPassed(seconds: number) {
-        setAmountSecondsPassed(seconds)
+  const { cycles, activeCycleId } = cyclesState
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
     }
 
-    function markCurrentCycleAsFinished() {
-        setCycles((state) => state.map((cycle) => {
-            if(cycle.id === activeCycleId) {
-                return {...cycle, finishedDate: new Date()}
-            } else {
-                return cycle
-            }
-        }),
-        )
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function markCurrentCycleAsFinished() {
+    dispatch(markCurrentCycleAsFinishedAction())
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
-    function createNewCycle(data: CreateCycleData) {
-        const id = String(new Date().getTime());
+    dispatch(addNewCycleAction(newCycle))
 
-        const newCycle: Cycle = {
-            id,
-            task: data.task,
-            minutesAmount: data.minutesAmount,
-            startDate: new Date(),
-        }
+    setAmountSecondsPassed(0)
+  }
 
-        setCycles((state) => [...state, newCycle])
-        setActiveCycleId(id)
-        setAmountSecondsPassed(0)
-        // reset()
-    }
+  function interruptCurrentCycle() {
+    dispatch(interruptCurrentCycleAction())
+  }
 
-    function interruptCurrentCycle() {
-        setCycles((state) =>
-            state.map((cycle) => {
-                if (cycle.id === activeCycleId) {
-                    return { ...cycle, interruptedDate: new Date() }
-                } else {
-                    return cycle
-                }
-            }),
-        )
-        setActiveCycleId(null)
-    }
-
-    return (
-        <CyclesContext.Provider 
-            value={{
-                activeCycle, 
-                activeCycleId, 
-                markCurrentCycleAsFinished, 
-                amountSecondsPassed, 
-                setSecondsPassed,
-                createNewCycle,
-                interruptCurrentCycle,
-                cycles}}>
-                    {children}
-        </CyclesContext.Provider>
-    )
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
 }
-
--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,14 +1,17 @@
+
 import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/esm/locale/pt-BR/index";
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
-    const { cycles } = useContext(CyclesContext)
+    const { cycles } = useContext(CyclesContext) 
     
     return (
         <HistoryContainer>
             <h1>Meu Histórico</h1>
-
+            
             <HistoryList>
                 <table>
                     <thead>
@@ -20,37 +23,31 @@ export function History() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                       
+                        {cycles.map(cycle => {
+                            return (
+                                <tr key={cycle.id}>
+                                    <td>{cycle.task}</td>
+                                    <td>{cycle.minutesAmount} minutos</td>
+                                    <td>{formatDistanceToNow(new Date(cycle.startDate), {
+                                        addSuffix: true,
+                                        locale: ptBR,
+                                    })}
+                                    </td>
+                                    <td>
+                                        {cycle.finishedDate && (
+                                            <Status statusColor="green">Concluído</Status>
+                                        )}
+                                        {cycle.interruptedDate && (
+                                            <Status statusColor="red">Interrompido</Status>
+                                        )}
+
+                                        {!cycle.finishedDate && !cycle.interruptedDate && (
+                                            <Status statusColor="yellow">Em andamento</Status>
+                                        )}
+                                    </td>
+                                </tr>
+                            )
+                        })}                       
                     </tbody>
                 </table>
             </HistoryList>
